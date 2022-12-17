097e7411304c74b2c1cdd87e173e52ad6031d4bd
CodeStarr <starr_x@163.com>
2022-12-17 17:12:15 +0800

    pycryptodome md5

--- Hash/main.c
+++ Hash/main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-
+#include "md5/md5.h"
 
 int main(int argc, char* argv[])
 {
-	
+	test_md5();
 	getchar();
 	return 0;
 }
---
+++ Hash/md5/md5.h
@@ -0,0 +1,5 @@
+#ifndef _MD5_H
+#define _MD5_H
+
+void test_md5();
+#endif
