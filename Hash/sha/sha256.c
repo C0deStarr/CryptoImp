f3f3b9135ebe675bc40e1915d675e7e387e0ff52
CodeStarr <starr_x@163.com>
2022-12-23 15:23:45 +0800

    SHA256_update

--- Hash/sha/sha256.c
+++ Hash/sha/sha256.c
@@ -30,10 +30,48 @@ ErrCrypto SHA256_init(HashState* pHashState)
     return errRet;
 }
 
+
+
+ErrCrypto AddBitsLen(HashState* pHashState, uint16_t nBits)
+{
+    // Maximum message length is 2**64 bits 
+    pHashState->nBitsLen += nBits;
+    return (pHashState->nBitsLen < nBits) ? ERR_MAX_DATA : ERR_OK;
+}
+
+
 ErrCrypto SHA256_update(HashState* pHashState, const uint8_t* pBuf, uint64_t nLen)
 {
     ErrCrypto errRet = ERR_OK;
+    uint8_t nBytesNeeded = 0;
+    uint8_t nBytesCopy = 0;
 
+    if (!pHashState || !pBuf)
+        return ERR_NULL;
+
+    while (nLen > 0)
+    {
+        nBytesNeeded = BLOCK_SIZE - pHashState->nBytesLen;
+        nBytesCopy = (nBytesNeeded > nLen) ? nLen : nBytesNeeded;
+        memcpy(pHashState->block, pBuf, nBytesCopy);
+        pBuf += nBytesCopy;
+        pHashState->nBytesLen += nBytesCopy;
+        nLen -= nBytesCopy;
+
+        if (BLOCK_SIZE == pHashState->nBytesLen)
+        {
+            // let's do the 80 steps
+            errRet = sha256_compress(pHashState);
+            if (errRet)
+                return errRet;
+
+            // waiting for the next block
+            pHashState->nBytesLen = 0;
+            errRet = AddBitsLen(pHashState, BLOCK_SIZE * 8);
+            if (errRet)
+                return errRet;
+        }
+    }
     return errRet;
 }
 
@@ -50,4 +88,5 @@ void test_sha256()
     ErrCrypto err = ERR_OK;
     uint8_t data[] = "abcde";
     SHA256_init(&hashState);
+    SHA256_update(&hashState, data, sizeof(data) - 1);
 }
