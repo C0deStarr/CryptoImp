0373a3a10f9ec364554d859b451b9578ccfd515b
CodeStarr <starr_x@163.com>
2022-12-29 10:55:58 +0800

    keccak_init()

--- Hash/sha/keccak.c
+++ Hash/sha/keccak.c
@@ -1,22 +1,33 @@
 
 #include "./keccak.h"
 
-ErrCrypto keccak_init(KeccakState* pKeccakState)
+
+ErrCrypto keccak_init(KeccakState* pKeccakState, uint32_t c)
 {
 	ErrCrypto err = ERR_OK;
+	if(!pKeccakState)
+		return ERR_NULL;
 
+	if(c >= KECCAK_b_200BYTES)
+		return ERR_DIGEST_SIZE;
+	pKeccakState->nByCapacity = c;
+	pKeccakState->nByRate = KECCAK_b_200BYTES - c;
+
+	pKeccakState->nr = 24;
 	return err;
 }
 ErrCrypto keccak_update(KeccakState* pKeccakState, const uint8_t* pBuf, uint64_t nLen)
 {
 	ErrCrypto err = ERR_OK;
-
+	if (!pKeccakState || !pBuf)
+		return ERR_NULL;
 	return err;
 }
 ErrCrypto keccak_digest(KeccakState* pKeccakState, uint8_t* pDigest, int nDigest)
 {
 	ErrCrypto err = ERR_OK;
-
+	if (!pKeccakState || !pDigest)
+		return ERR_NULL;
 	return err;
 }
 
@@ -26,7 +37,7 @@ void test_keccak()
 
 	uint8_t data[] = "abc";
 	uint8_t digest[MAX_MD_SIZE] = {0};
-	uint8_t nDigest = 224;
+	uint8_t nDigest = 28;
 	uint8_t i = 0;
 	keccak_init(&keccakState);
 	keccak_update(&keccakState, data, sizeof(data) - 1);
--- Hash/sha/keccak.h
+++ Hash/sha/keccak.h
@@ -4,18 +4,36 @@
 
 #include <common/common.h>
 
-#define KECCAK_b1600bits_200bytes	200
+/*
+* l = 6, w = 64, b = 1600
+*/
+
+#define KECCAK_b_200BYTES	200	// 1600/8
 #define MAX_MD_SIZE	64	// sha3-512
+#define NUMBER_OF_ROUNDS 24	// for keccak-f, number of round == 12 + 2*l == 24
+
 typedef struct
 {
 	// w == b / 25 == length of lane == 8 bytes == sizeof(uint64_t)
 	uint64_t ullArrStateLanes[5][5];
 
 	// as long as the array state
-	uint8_t buf[KECCAK_b1600bits_200bytes];
+	uint8_t buf[KECCAK_b_200BYTES];
+
+	// c + r == b
+	uint32_t nByCapacity;
+	uint32_t nByRate;
+
+	// number of round
+	uint32_t nr;
 }KeccakState;
 
-ErrCrypto keccak_init(KeccakState* pKeccakState);
+/*
+*
+* args:
+*	c: number of bytes of capacity
+*/
+ErrCrypto keccak_init(KeccakState* pKeccakState, uint32_t c /*, uint32_t nr = 24*/);
 ErrCrypto keccak_update(KeccakState* pKeccakState, const uint8_t* pBuf, uint64_t nLen);
 ErrCrypto keccak_digest(KeccakState* pKeccakState, uint8_t* pDigest, int nDigest);
 void test_keccak();
