32e95a40abf75e4c63c7507f5c464cbf695256df
CodeStarr <starr_x@163.com>
2023-01-16 10:52:41 +0800

    aes_init()

--- Cipher/BlockCipher/aes.c
+++ Cipher/BlockCipher/aes.c
@@ -1,1 +1,52 @@
 #include "aes.h"
+
+
+ErrCrypto aes_init(StcAES* pStcAES, aes_key_size nAesKeySize, uint8_t* pKey, uint32_t nKey)
+{
+	ErrCrypto errRet = ERR_OK;
+	if (!pStcAES || !pKey)
+	{
+		return ERR_NULL;
+	}
+
+	if (nKey != (nAesKeySize / 8))
+	{
+		return ERR_KEY_SIZE;
+	}
+
+	switch (nAesKeySize)
+	{
+		case aes128:
+		{
+			pStcAES->Nk = 4;
+			pStcAES->Nr = 10;
+		}
+		break;
+		case aes192:
+		{
+			pStcAES->Nk = 6;
+			pStcAES->Nr = 12;
+		}
+		break;
+		case aes256:
+		{
+			pStcAES->Nk = 8;
+			pStcAES->Nr = 14;
+		}
+		break;
+		default:
+			return ERR_KEY_SIZE;
+	}
+	pStcAES->nKeySize = nAesKeySize;
+	pStcAES->Nb = AES_Nb;
+
+	errRet = KeyExpansion(pKey, pStcAES->w, pStcAES->Nk);
+
+	return errRet;
+}
+
+ErrCrypto KeyExpansion(uint8_t key[/*4*Nk*/], uint32_t w[/*Nb*(Nr+1)*/], uint32_t Nk)
+{
+
+}
+
--- Cipher/BlockCipher/aes.h
+++ Cipher/BlockCipher/aes.h
@@ -2,6 +2,28 @@
 #define _AES_H
 
 
+#include <common/common.h>
+
+#define AES_BLOCK_SIZE 16
+#define AES_Nb 4	// == number of state columns == AES_BLOCK_SIZE / sizeof(dword)
+
+typedef enum {
+	aes128 = 128,	// Nr = 10; Nk = 4 dword
+	aes192 = 192,	// Nr = 12; Nk = 6 dword
+	aes256 = 256	// Nr = 14; Nk = 8 dword
+}aes_key_size;
+
+typedef struct {
+	uint32_t w[60];	// max == Nb
+	aes_key_size nKeySize;
+	uint32_t Nb;
+	uint32_t Nr;
+	uint32_t Nk;
+}StcAES;
+
+ErrCrypto aes_init(StcAES* pStcAES, aes_key_size nAesKeySize, uint8_t *pKey, uint32_t nKey);
+ErrCrypto KeyExpansion(uint8_t key[/*4*Nk*/],  uint32_t w[/*Nb*(Nr+1)*/], uint32_t Nk);
+
 
 #endif
 
