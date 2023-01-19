ce556010a16cb445ba3bcf191a7a67c822d9d10c
CodeStarr <starr_x@163.com>
2023-01-19 10:57:50 +0800

    key expansion

--- Cipher/BlockCipher/sm4.h
+++ Cipher/BlockCipher/sm4.h
@@ -7,23 +7,22 @@
 #define SM4_BLOCK_SIZE 16
 #define SM4_Nb 4	// == number of state columns == SM4_BLOCK_SIZE / sizeof(dword)
 #define SM4_KEY_SIZE 16
+#define SM4_ROUNDS 32
 
 
 typedef struct {
-	uint32_t w[60];	// max == Nb * (Nr+1)
-	uint32_t dw[60];	// max == Nb * (Nr+1)
+	uint32_t rk[SM4_ROUNDS];
+}sm4_ctx;
 
-}StcSM4;
+ErrCrypto sm4_init(sm4_ctx* pStcSM4, uint8_t *pKey, uint32_t nKey);
 
-ErrCrypto sm4_init(StcSM4* pStcSM4, uint8_t *pKey, uint32_t nKey);
-
-ErrCrypto sm4_encrypt(StcSM4* pStcSM4
+ErrCrypto sm4_encrypt(sm4_ctx* pStcSM4
 	, uint8_t *in
 	, uint32_t nIn/* = SM4_BLOCK_SIZE*/
 	, uint8_t* pOut
 	, uint32_t nOut/* = SM4_BLOCK_SIZE*/);
 
-ErrCrypto sm4_decrypt(StcSM4* pStcSM4
+ErrCrypto sm4_decrypt(sm4_ctx* pStcSM4
 	, uint8_t* in
 	, uint32_t nIn/* = SM4_BLOCK_SIZE*/
 	, uint8_t* pOut
