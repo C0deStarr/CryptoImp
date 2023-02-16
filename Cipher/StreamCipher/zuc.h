08ba1df2896f129699643f51e5a2f7c8e051cbe6
CodeStarr <starr_x@163.com>
2023-02-16 16:29:10 +0800

    ZUC key generator

--- Cipher/StreamCipher/zuc.h
+++ Cipher/StreamCipher/zuc.h
@@ -1,6 +1,29 @@
 #ifndef _ZUC_H
 #define _ZUC_H
 
+#include <common/common.h>
+
+
+# define ZUC_KEY_SIZE	16
+# define ZUC_IV_SIZE	16
+
+typedef struct {
+	uint32_t LFSR[16];
+	uint32_t R1;
+	uint32_t R2;
+} ZUC;
+
+ErrCrypto zuc_init(ZUC* pState
+	, const uint8_t *pKey, uint32_t nKey/* = ZUC_KEY_SIZE*/
+	, const uint8_t *pIV, uint32_t nIV/* =ZUC_IV_SIZE*/
+);
+
+ErrCrypto zuc_generate_keystream(ZUC* pState
+	, uint32_t nWords
+	, uint32_t* keystream);
+
+
+
 
 void test_zuc();
 
